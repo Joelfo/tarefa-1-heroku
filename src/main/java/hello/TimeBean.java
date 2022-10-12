9b9afae5a5391db98b6c94a13c05b7f0221185c3
Joel Lopes <joellopessouza@id.uff.br>
2022-10-11 21:24:17 -0300

    test

--- src/main/java/hello/TimeBean.java
+++ src/main/java/hello/TimeBean.java
@@ -18,7 +18,7 @@ public class TimeBean implements Serializable {
     private String period;
     
     public TimeBean() {
-       setTime((float) Calendar.getInstance(TimeZone.getDefault()).get(Calendar.HOUR_OF_DAY));
+       setTime(Calendar.getInstance(TimeZone.getDefault()).get(Calendar.HOUR_OF_DAY));
     }
     
     public float getTime(){
